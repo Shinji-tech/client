de8ddd8c5a984d682d3e55fe58de6055b09c50b2
BuiQuangGiau1 <114122977+BuiQuangGiau1@users.noreply.github.com>
2023-12-14 01:15:00 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
+// Get the root element from the HTML document
 const rootElement = document.getElementById('root');
 
 // Replace ReactDOM.render with createRoot().render
